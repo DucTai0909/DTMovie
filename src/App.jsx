276e0905262ea03e166f19a1c0b177494e41d58b
Tài Nguyễn Tô Đức <nguyentoductai@gmail.com>
2024-10-07 01:01:17 +0700

    MovieList

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,40 @@
+import { useEffect, useState } from "react";
 import Banner from "./components/Banner";
 import Header from "./components/Header";
+import MovieList from "./components/MovieList";
 
 function App() {
-  return (
-    <>
-      <div>
-        <Header />
-        <Banner />
-      </div>
-    </>
-  );
+    const [movie, setMovie] = useState([]);
+
+    useEffect(() => {
+        const fetchMovie = async () => {
+            const options = {
+                method: "GET",
+                headers: {
+                    accept: "application/json",
+                    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
+                },
+            };
+            const url =
+                "https://api.themoviedb.org/3/movie/popular?language=vi&page=1";
+            const response = await fetch(url, options);
+            const data = await response.json();
+            
+            console.log(data);
+          }
+          fetchMovie();
+    }, []);
+
+    return (
+        <>
+            <div className="bg-black pb-10">
+                <Header />
+                <Banner />
+                <MovieList title={"Phim Hot"} />
+                <MovieList title={"Phim đề cử"} />
+            </div>
+        </>
+    );
 }
 
 export default App;

b265fa67900a24ce6de453d3142366d06e3972cc
Tài Nguyễn Tô Đức <nguyentoductai@gmail.com>
2024-10-08 00:04:44 +0700

    cm

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,11 @@ import { useEffect, useState } from "react";
 import Banner from "./components/Banner";
 import Header from "./components/Header";
 import MovieList from "./components/MovieList";
+import { data } from "autoprefixer";
 
 function App() {
     const [movie, setMovie] = useState([]);
+    const [movieRate, setMovieRate] = useState([]);
 
     useEffect(() => {
         const fetchMovie = async () => {
@@ -15,12 +17,20 @@ function App() {
                     Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
                 },
             };
-            const url =
+            const url1 =
                 "https://api.themoviedb.org/3/movie/popular?language=vi&page=1";
-            const response = await fetch(url, options);
-            const data = await response.json();
-            
-            console.log(data);
+            const url2 = 'https://api.themoviedb.org/3/movie/top_rated?language=vi&page=1';
+
+            const [res1, res2] = await Promise.all([
+                fetch(url1, options),
+                fetch(url2, options),
+            ])
+
+            const data1 = await res1.json();
+            const data2 = await res2.json();
+
+            setMovie(data1.results);
+            setMovieRate(data2.results);
           }
           fetchMovie();
     }, []);
@@ -30,8 +40,8 @@ function App() {
             <div className="bg-black pb-10">
                 <Header />
                 <Banner />
-                <MovieList title={"Phim Hot"} />
-                <MovieList title={"Phim đề cử"} />
+                <MovieList title={"Phim Hot"} data={movie} />
+                <MovieList title={"Phim đề cử"} data={movieRate} />
             </div>
         </>
     );

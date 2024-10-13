a258402e25ca11cc33e8e20d0643d29cb09f9c25
Tài Nguyễn Tô Đức <nguyentoductai@gmail.com>
2024-10-13 23:07:00 +0700

    final and deploy on vercel

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,32 +1,46 @@
-import React from "react";
+import { useState } from "react";
+import PropType from "prop-types";
+const Header = ({ onSearch }) => {
+    const [textSearch, setTextSearch] = useState("");
+    return (
+        <div className="p-4 bg-black flex items-center justify-between">
+            <div className="flex items-center space-x-4">
+                <h1 className="text-[30px] uppercase font-bold text-red-700">
+                    Movie
+                </h1>
+                <nav className="flex items-center space-x-4">
+                    <a href="" className="text-white">
+                        Home
+                    </a>
+                    <a href="" className="text-white">
+                        About
+                    </a>
+                    <a href="" className="text-white">
+                        Contact
+                    </a>
+                </nav>
+            </div>
+            <div className="flex items-center space-x-4">
+                <input
+                    type="text"
+                    placeholder="Search movie name"
+                    className="p-3 text-black"
+                    onChange={(e) => setTextSearch(e.target.value)}
+                    value={textSearch}
+                />
+                <button
+                    onClick={() => onSearch(textSearch)}
+                    className="p-2 text-white bg-red-600"
+                >
+                    Search
+                </button>
+            </div>
+        </div>
+    );
+};
 
-const Header = () => {
-  return (
-    <div className="p-4 bg-black flex items-center justify-between">
-      <div className="flex items-center space-x-4">
-        <h1 className="text-[30px] uppercase font-bold text-red-700">Movie</h1>
-        <nav className="flex items-center space-x-4">
-          <a href="" className="text-white">
-            Home
-          </a>
-          <a href="" className="text-white">
-            About
-          </a>
-          <a href="" className="text-white">
-            Contact
-          </a>
-        </nav>
-      </div>
-      <div className="flex items-center space-x-4">
-        <input
-          type="text"
-          placeholder="Search movie name"
-          className="p-3 text-black"
-        />
-        <button className="p-2 text-white bg-red-600">Search</button>
-      </div>
-    </div>
-  );
+Header.propTypes = {
+    onSearch: PropType.func,
 };
 
 export default Header;
--- src/components/MovieList.jsx
+++ src/components/MovieList.jsx
@@ -3,18 +3,8 @@ import ImgTemp from "../assets/temp-1.jpeg";
 import { data } from "autoprefixer";
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
-import Modal from "react-modal";
-import { useState } from "react";
-import YouTube from "react-youtube";
-
-const opts = {
-    height: "390",
-    width: "640",
-    playerVars: {
-        // https://developers.google.com/youtube/player_parameters
-        autoplay: 1,
-    },
-};
+import { useContext } from "react";
+import { MovieContext } from "../context/MovieProvider";
 
 const responsive = {
     superLargeDesktop: {
@@ -37,29 +27,7 @@ const responsive = {
 };
 
 const MovieList = ({ title, data }) => {
-    const [modalIsOpen, setModalIsOpen] = useState(false);
-    const [trailerKey, setTrailerKey] = useState("");
-
-    const handleTrailer = async (id) => {
-        setTrailerKey("");
-        try {
-            const url = `https://api.themoviedb.org/3/movie/${id}/videos?language=en-US`;
-            const options = {
-                method: "GET",
-                headers: {
-                    accept: "application/json",
-                    Authorization: `Bearer ${import.meta.env.VITE_API_KEY}`,
-                },
-            };
-            const moviekey = await fetch(url, options);
-            const data = await moviekey.json();
-            setTrailerKey(data.results[0].key);
-            setModalIsOpen(true);
-        } catch (error) {
-            setModalIsOpen(false);
-            console.log(error);
-        }
-    };
+    const { handleTrailer } = useContext(MovieContext);
 
     return (
         <div className="text-white p-10 mb-10">
@@ -68,7 +36,8 @@ const MovieList = ({ title, data }) => {
                 responsive={responsive}
                 className="flex items-center space-x-4"
             >
-                {data.length > 0 &&
+                {data &&
+                    data.length > 0 &&
                     data.map((item) => (
                         <div
                             key={item.id}
@@ -96,30 +65,6 @@ const MovieList = ({ title, data }) => {
                         </div>
                     ))}
             </Carousel>
-            <Modal
-                isOpen={modalIsOpen}
-                onRequestClose={() => setModalIsOpen(false)}
-                style={{
-                    overlay: {
-                        position : 'fixed',
-                        zIndex: 9999,
-                    },
-                    content : {
-                        top: "50%",
-                        left: "50%",
-                        right: "auto",
-                        bottom: "auto",
-                        marginRight: "-50%",
-                        transform: "translate(-50%, -50%)",
-                    },
-                }}
-                contentLabel="Example Modal"
-            >
-                <YouTube
-                    videoId={trailerKey}
-                    opts={opts}
-                />
-            </Modal>
         </div>
     );
 };
